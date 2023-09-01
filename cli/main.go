9f1391d61fef47b07ef72f927e109e4e34965de4
fabiante <fabiante@users.noreply.github.com>
2023-09-01 16:17:46 +0200

    Fix CLI main not using sqlite

--- cli/main.go
+++ cli/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/fabiante/persurl/api"
 	"github.com/gin-gonic/gin"
+	_ "modernc.org/sqlite"
 )
 
 // main is a crude CLI entrypoint for the application. Will be replaced
 // with a proper CLI supporting multiple commands later.
 func main() {
 	engine := gin.Default()
-	server := api.NewServer()
+
+	sqlitePath := "./prod.sqlite"
+
+	database, err := sql.Open("sqlite", sqlitePath)
+	if err != nil {
+		log.Fatalf("opening sqlite database failed: %s", err)
+	}
+
+	server := api.NewServer(database)
 	api.SetupRouting(engine, server)
 	if err := engine.Run(":8060"); err != nil {
 		log.Fatalf("running api failed: %s", err)
