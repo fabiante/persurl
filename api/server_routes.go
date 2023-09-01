76a736c63fa6d6f322179eba69cdebed93697b99
fabiante <fabiante@users.noreply.github.com>
2023-09-01 17:06:17 +0200

    Refactor naming of resolve and admin types
    This should improve maintainability.

--- api/server_routes.go
+++ api/server_routes.go
@@ -3,7 +3,9 @@ package api
 import "github.com/gin-gonic/gin"
 
 func SetupRouting(r gin.IRouter, s *Server) {
+	// Resolve endpoints
 	r.GET("/r/:domain/:name", s.Resolve)
 
+	// Admin endpoints
 	r.PUT("/a/domains/:domain/purls/:name", s.Save)
 }
