4246d4e951fbf6b1ae0319cf389528df07ee3886
fabiante <fabiante@users.noreply.github.com>
2023-09-01 16:14:51 +0200

    Refactor API tests to properly setup db

--- tests/http_test.go
+++ tests/http_test.go
@@ -1,21 +1,39 @@
 package tests
 
 import (
+	"database/sql"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/fabiante/persurl/api"
 	"github.com/fabiante/persurl/tests/driver"
 	"github.com/fabiante/persurl/tests/specs"
 	"github.com/gin-gonic/gin"
+	_ "modernc.org/sqlite"
 )
 
 func TestWithHTTPDriver(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	handler := gin.Default()
 
-	server := api.NewServer()
+	sqlitePath := "./mydb.sqlite"
+
+	// ensure file does not exist
+	_ = os.Remove(sqlitePath)
+
+	db, err := sql.Open("sqlite", sqlitePath)
+	if err != nil {
+		panic(err)
+	}
+
+	err = migrateDb(db)
+	if err != nil {
+		panic(err)
+	}
+
+	server := api.NewServer(db)
 	api.SetupRouting(handler, server)
 
 	testServer := httptest.NewServer(handler)
@@ -24,4 +42,34 @@ func TestWithHTTPDriver(t *testing.T) {
 
 	specs.TestResolver(t, dr)
 	specs.TestAdministration(t, dr)
+}
+
+func migrateDb(db *sql.DB) error {
+	stmts := []string{
+		`create table purls
+(
+    id     integer       not null
+        constraint puls_pk
+            primary key autoincrement,
+    domain varchar(32)   not null,
+    name   varchar(128)  not null,
+    target varchar(4096) not null,
+    constraint puls_pk2
+        unique (name, domain)
+);
+
+create index puls_domain_index
+    on purls (domain);
+
+`,
+	}
+
+	for _, stmt := range stmts {
+		_, err := db.Exec(stmt)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
