e137f75b6c71908a088c6ee444827a9de16f6b4b
fabiante <fabiante@users.noreply.github.com>
2023-09-02 09:25:25 +0200

    Normalize SQL table purls into two tables (domains, purls)
    This makes the data model more relational with the usual benefits.

--- db/migrate.go
+++ db/migrate.go
@@ -1,31 +1,43 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 func MigrateDb(db *sql.DB) error {
 	stmts := []string{
-		`create table if not exists purls
+		`create table main.domains
 (
-    id     integer       not null
+    id   integer      not null
+        constraint domains_pk
+            primary key autoincrement,
+    name varchar(128) not null
+        constraint domains_pk2
+            unique
+)
+`,
+		`
+create table purls
+(
+    id        integer       not null
         constraint puls_pk
             primary key autoincrement,
-    domain varchar(32)   not null,
-    name   varchar(128)  not null,
-    target varchar(4096) not null,
-    constraint puls_pk2
-        unique (name, domain)
-);
-
-create index puls_domain_index
-    on purls (domain);
-
-`,
+    domain_id integer       not null
+        constraint purls_domains_id_fk
+            references domains
+            on delete restrict,
+    name      varchar(128)  not null,
+    target    varchar(4096) not null,
+    constraint purls_pk
+        unique (domain_id, name)
+);`,
 	}
 
-	for _, stmt := range stmts {
+	for i, stmt := range stmts {
 		_, err := db.Exec(stmt)
 		if err != nil {
-			return err
+			return fmt.Errorf("stmts[%d] failed: %w", i, err)
 		}
 	}
 
