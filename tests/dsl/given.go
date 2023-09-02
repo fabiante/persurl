dc04b2c2df23c32a2b554bf0a134073f428a3dc6
fabiante <fabiante@users.noreply.github.com>
2023-09-02 09:27:23 +0200

    Add GivenExistingDomain for test specs

--- tests/dsl/given.go
+++ tests/dsl/given.go
@@ -12,8 +12,16 @@ type AdminAPI interface {
 }
 
 // GivenExistingPURL ensures that a PURL is known to the application.
+//
 // This is done by simply creating it.
 func GivenExistingPURL(t *testing.T, service AdminAPI, purl *PURL) {
 	err := service.CreatePurl(purl)
 	require.NoError(t, err, "creating purl failed")
+}
+
+// GivenExistingDomain ensures that a Domain is known to the application.
+//
+// This currently is a no-op since domains can't explicitly be created.
+func GivenExistingDomain(t *testing.T, service AdminAPI, domain string) {
+	// no-op - Implement something here once domains can be created.
 }
