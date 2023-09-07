1675568c117219e5ae6ad7fe3afe69f22bb07edc
fabiante <fabiante@users.noreply.github.com>
2023-09-07 14:43:07 +0200

    Refactor admin tests in separate functions

--- tests/specs/admin.go
+++ tests/specs/admin.go
@@ -10,75 +10,82 @@ import (
 
 func TestAdministration(t *testing.T, admin dsl.AdminAPI) {
 	t.Run("administration", func(t *testing.T) {
-		t.Run("can't create invalid domain", func(t *testing.T) {
-			invalid := []string{
-				// empty
-				"",
-				// whitespace
-				"a b",
-				// url encoded whitespace
-				"a%20b",
-				// random characters
-				"^",
-				"~",
-				":",
-				",",
-				"`",
-			}
+		testDomainAdmin(t, admin)
+		testPurlAdmin(t, admin)
+	})
+}
 
-			for i, domain := range invalid {
-				t.Run(fmt.Sprintf("invalid[%d]", i), func(t *testing.T) {
-					err := admin.CreateDomain(domain)
-					require.Error(t, err)
-					//require.ErrorIs(t, err, app.ErrBadRequest) // TODO: Some tests cause a 404 with the http driver.
-				})
-			}
-		})
+func testPurlAdmin(t *testing.T, admin dsl.AdminAPI) {
+	t.Run("can't create invalid PURL", func(t *testing.T) {
+		invalid := []*dsl.PURL{
+			// empty
+			dsl.NewPURL("valid", "", mustParseURL("example.com")),
+			// whitespace
+			dsl.NewPURL("valid", "a b", mustParseURL("example.com")),
+			// url encoded whitespace
+			dsl.NewPURL("valid", "a%20b", mustParseURL("example.com")),
+			// random characters
+			dsl.NewPURL("valid", "^", mustParseURL("example.com")),
+			dsl.NewPURL("valid", "~", mustParseURL("example.com")),
+			dsl.NewPURL("valid", ":", mustParseURL("example.com")),
+			dsl.NewPURL("valid", ",", mustParseURL("example.com")),
+			dsl.NewPURL("valid", "`", mustParseURL("example.com")),
+		}
 
-		t.Run("can't create invalid PURL", func(t *testing.T) {
-			invalid := []*dsl.PURL{
-				// empty
-				dsl.NewPURL("valid", "", mustParseURL("example.com")),
-				// whitespace
-				dsl.NewPURL("valid", "a b", mustParseURL("example.com")),
-				// url encoded whitespace
-				dsl.NewPURL("valid", "a%20b", mustParseURL("example.com")),
-				// random characters
-				dsl.NewPURL("valid", "^", mustParseURL("example.com")),
-				dsl.NewPURL("valid", "~", mustParseURL("example.com")),
-				dsl.NewPURL("valid", ":", mustParseURL("example.com")),
-				dsl.NewPURL("valid", ",", mustParseURL("example.com")),
-				dsl.NewPURL("valid", "`", mustParseURL("example.com")),
-			}
+		dsl.GivenExistingDomain(t, admin, "valid")
 
-			dsl.GivenExistingDomain(t, admin, "valid")
+		for i, purl := range invalid {
+			t.Run(fmt.Sprintf("invalid[%d]", i), func(t *testing.T) {
+				err := admin.SavePURL(purl)
+				require.Error(t, err)
+				//require.ErrorIs(t, err, app.ErrBadRequest) // TODO: Some tests cause a 404 with the http driver.
+			})
+		}
+	})
 
-			for i, purl := range invalid {
-				t.Run(fmt.Sprintf("invalid[%d]", i), func(t *testing.T) {
-					err := admin.SavePURL(purl)
-					require.Error(t, err)
-					//require.ErrorIs(t, err, app.ErrBadRequest) // TODO: Some tests cause a 404 with the http driver.
-				})
-			}
-		})
+	t.Run("can create new PURL", func(t *testing.T) {
+		domain := "my-domain-123456"
+		purl := dsl.NewPURL(domain, "my-name3456345663456", mustParseURL("https://google.com"))
 
-		t.Run("can create new PURL", func(t *testing.T) {
-			domain := "my-domain-123456"
-			purl := dsl.NewPURL(domain, "my-name3456345663456", mustParseURL("https://google.com"))
+		dsl.GivenExistingDomain(t, admin, domain)
+		// TODO: Assert non-existence of purl to be created
+		dsl.GivenExistingPURL(t, admin, purl)
+	})
 
-			dsl.GivenExistingDomain(t, admin, domain)
-			// TODO: Assert non-existence of purl to be created
-			dsl.GivenExistingPURL(t, admin, purl)
-		})
+	t.Run("can update existing purl", func(t *testing.T) {
+		domain := "my-domain-123456789"
+		purl := dsl.NewPURL(domain, "my-name3458904562454564565467", mustParseURL("https://google.com"))
 
-		t.Run("can update existing purl", func(t *testing.T) {
-			domain := "my-domain-123456789"
-			purl := dsl.NewPURL(domain, "my-name3458904562454564565467", mustParseURL("https://google.com"))
+		dsl.GivenExistingDomain(t, admin, domain)
+		dsl.GivenExistingPURL(t, admin, purl)
 
-			dsl.GivenExistingDomain(t, admin, domain)
-			dsl.GivenExistingPURL(t, admin, purl)
+		require.NoError(t, admin.SavePURL(purl), "updating existing purl failed")
+	})
+}
 
-			require.NoError(t, admin.SavePURL(purl), "updating existing purl failed")
-		})
+func testDomainAdmin(t *testing.T, admin dsl.AdminAPI) {
+	t.Run("can't create invalid domain", func(t *testing.T) {
+		invalid := []string{
+			// empty
+			"",
+			// whitespace
+			"a b",
+			// url encoded whitespace
+			"a%20b",
+			// random characters
+			"^",
+			"~",
+			":",
+			",",
+			"`",
+		}
+
+		for i, domain := range invalid {
+			t.Run(fmt.Sprintf("invalid[%d]", i), func(t *testing.T) {
+				err := admin.CreateDomain(domain)
+				require.Error(t, err)
+				//require.ErrorIs(t, err, app.ErrBadRequest) // TODO: Some tests cause a 404 with the http driver.
+			})
+		}
 	})
 }
