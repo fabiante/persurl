591032a4c1cecb80b01e27e5dddecc4d97cb8484
fabiante <fabiante@users.noreply.github.com>
2023-09-14 12:27:01 +0200

    Replace in-mem service usage with new db.Database in tests and CLI

--- cli/main.go
+++ cli/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"log"
 
+	"github.com/doug-martin/goqu/v9"
 	"github.com/fabiante/persurl/api"
-	"github.com/fabiante/persurl/app"
+	"github.com/fabiante/persurl/db"
 	"github.com/gin-gonic/gin"
 )
 
 // main is a crude CLI entrypoint for the application. Will be replaced
 // with a proper CLI supporting multiple commands later.
 func main() {
+	database, err := db.SetupDB("./prod.sqlite")
+	if err != nil {
+		log.Fatalf("setting up database failed: %s", err)
+	}
+
 	engine := gin.Default()
-	service := app.NewService()
+	service := db.NewDatabase(goqu.New("sqlite3", database))
 	server := api.NewServer(service)
 	api.SetupRouting(engine, server)
 	if err := engine.Run(":8060"); err != nil {
