9361a2d9053edd2b8975ce9f438c1ff7f3e6dfb2
fabiante <fabiante@users.noreply.github.com>
2023-09-15 08:53:41 +0200

    Add dedicated load test entrypoint

---
+++ tests/http_load_test.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/doug-martin/goqu/v9"
+	"github.com/fabiante/persurl/api"
+	"github.com/fabiante/persurl/db"
+	"github.com/fabiante/persurl/tests/driver"
+	"github.com/fabiante/persurl/tests/specs"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadWithHTTPDriver(t *testing.T) {
+	if os.Getenv("TEST_LOAD") == "" {
+		t.Skip("load tests are skipped because TEST_LOAD env variable is not set")
+	}
+
+	gin.SetMode(gin.TestMode)
+	handler := gin.Default()
+
+	sqlitePath := "./test_load_http.sqlite"
+	_ = os.Remove(sqlitePath) // remove to ensure a clean database
+	database, err := db.SetupDB(sqlitePath)
+	require.NoError(t, err, "setting up db failed")
+
+	service := db.NewDatabase(goqu.New("sqlite3", database))
+	server := api.NewServer(service)
+	api.SetupRouting(handler, server)
+
+	testServer := httptest.NewServer(handler)
+
+	dr := driver.NewHTTPDriver(testServer.URL, http.DefaultTransport)
+
+	specs.TestLoad(t, dr)
+}
