b90b0b8472244bf3d898651e418fec5ad6d193ee
fabiante <fabiante@users.noreply.github.com>
2023-09-18 17:58:18 +0200

    Remove sqlite from db package

--- db/setup.go
+++ db/setup.go
@@ -6,34 +6,9 @@ import (
 
 	"github.com/doug-martin/goqu/v9"
 	_ "github.com/doug-martin/goqu/v9/dialect/postgres"
-	_ "github.com/doug-martin/goqu/v9/dialect/sqlite3"
 	"github.com/fabiante/persurl/db/migrations"
 	_ "github.com/lib/pq"
-	_ "modernc.org/sqlite"
 )
-
-func SetupAndMigrateDB(path string) (*sql.DB, *goqu.Database, error) {
-	db, gdb, err := SetupDB(path)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = migrations.RunSQLite(db)
-	if err != nil {
-		return nil, nil, fmt.Errorf("migrating sqlite database failed: %s", err)
-	}
-
-	return db, gdb, nil
-}
-
-func SetupDB(path string) (*sql.DB, *goqu.Database, error) {
-	database, err := sql.Open("sqlite", path)
-	if err != nil {
-		return nil, nil, fmt.Errorf("opening sqlite database failed: %s", err)
-	}
-
-	return database, goqu.New("sqlite3", database), nil
-}
 
 func SetupAndMigratePostgresDB(dsn string) (*sql.DB, *goqu.Database, error) {
 	db, gdb, err := SetupPostgresDB(dsn)
