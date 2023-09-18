ef3bb177f1f935cf6e87984c9fc900aa7c15fe85
fabiante <fabiante@users.noreply.github.com>
2023-09-18 14:02:15 +0200

    Add postgres setup function

--- db/setup.go
+++ db/setup.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 
 	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/postgres"
 	_ "github.com/doug-martin/goqu/v9/dialect/sqlite3"
 	"github.com/fabiante/persurl/db/migrations"
+	_ "github.com/lib/pq"
 	_ "modernc.org/sqlite"
 )
 
@@ -31,4 +33,27 @@ func SetupDB(path string) (*sql.DB, *goqu.Database, error) {
 	}
 
 	return database, goqu.New("sqlite3", database), nil
+}
+
+func SetupAndMigratePostgresDB(dsn string) (*sql.DB, *goqu.Database, error) {
+	db, gdb, err := SetupPostgresDB(dsn)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = migrations.RunPostgres(db)
+	if err != nil {
+		return nil, nil, fmt.Errorf("migrating postgres database failed: %s", err)
+	}
+
+	return db, gdb, nil
+}
+
+func SetupPostgresDB(dsn string) (*sql.DB, *goqu.Database, error) {
+	database, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, nil, fmt.Errorf("opening postgres database failed: %s", err)
+	}
+
+	return database, goqu.New("postgres", database), nil
 }
