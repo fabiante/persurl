034c4f81b2a1d9a1e456b03bf09eca575e7c79c6
fabiante <fabiante@users.noreply.github.com>
2023-09-18 14:14:41 +0200

    Add migrations for postgres

--- db/migrations/migrate.go
+++ db/migrations/migrate.go
@@ -26,6 +26,25 @@ func RunSQLite(db *sql.DB) error {
 	return nil
 }
 
+func RunPostgres(db *sql.DB) error {
+	// Configure migrations
+
+	m, err := migrator.New(
+		// type cast is required because []*migrator.MigrationNoTx is not assignable to []migrator.Migration
+		migrator.Migrations(migrationsPostgres...),
+	)
+	if err != nil {
+		return fmt.Errorf("initializing migrations failed: %w", err)
+	}
+
+	// Migrate up
+	if err := m.Migrate(db); err != nil {
+		return fmt.Errorf("running migrations failed: %w", err)
+	}
+
+	return nil
+}
+
 func newMigration(name string, query string) *migrator.MigrationNoTx {
 	return &migrator.MigrationNoTx{
 		Name: name,
@@ -61,6 +80,34 @@ var migrationsSQLite = []any{
     name      varchar(128)  not null,
     target    varchar(4096) not null,
     constraint purls_pk
+        unique (domain_id, name)
+)`,
+	),
+}
+
+var migrationsPostgres = []any{
+	newMigration("2023-09-18-00000001-CreateTableDomains", `create table domains
+(
+    id   serial
+        constraint domains_pk2
+            unique,
+    name varchar(128) not null
+        constraint domains_pk
+            primary key
+)`,
+	),
+	newMigration("2023-09-18-00000002-CreateTablePurls", `create table purls
+(
+    id        serial
+        constraint purls_pk
+            primary key,
+    domain_id integer       not null
+        constraint purls_domains_id_fk
+            references domains (id)
+			on delete restrict,
+    name      varchar(128)  not null,
+    target    varchar(4096) not null,
+    constraint purls_pk2
         unique (domain_id, name)
 )`,
 	),
