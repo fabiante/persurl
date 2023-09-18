2cd1494d07c2f66051b77a044ff3c70862c5bf3b
fabiante <fabiante@users.noreply.github.com>
2023-09-18 17:48:57 +0200

    Add godotenv dep and add env loading helper

--- config/env.go
+++ config/env.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(fmt.Errorf("loading env failed: %w", err))
+	}
+}
 
 func DataDir() string {
 	dataDir := os.Getenv("PERSURL_DATA_DIR")
