94389232ce7237adc2eca3ae8c8639472737de67
fabiante <fabiante@users.noreply.github.com>
2023-09-21 15:03:16 +0200

    Add API to retrieve typed DB config from package

--- config/config.go
+++ config/config.go
@@ -1,8 +1,41 @@
 package config
 
+import "errors"
+
 // init initializes this packages state.
 func init() {
-	if err := initViper(); err != nil {
+	errs := []error{
+		initViper(),
+		initConfig(),
+	}
+
+	if err := errors.Join(errs...); err != nil {
 		panic(err)
 	}
+}
+
+// Config is the top level aggregator for all configurations.
+type Config struct {
+	DB *DB
+}
+
+var config *Config
+
+func initConfig() error {
+	config = &Config{
+		DB: &DB{},
+	}
+
+	errs := []error{
+		vip.UnmarshalKey("db", config.DB),
+	}
+
+	return errors.Join(errs...)
+}
+
+// Get retrieves the singleton Config of this application.
+//
+// You may store references to this config, but you shall not modify it.
+func Get() *Config {
+	return config
 }
