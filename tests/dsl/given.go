6728ac70a66714fad4121129253a26977bca5877
fabiante <fabiante@users.noreply.github.com>
2023-09-24 12:36:19 +0200

    Add Context object to Given funcs

--- tests/dsl/given.go
+++ tests/dsl/given.go
@@ -1,6 +1,7 @@
 package dsl
 
 import (
+	"context"
 	"testing"
 
 	"github.com/fabiante/persurl/app"
@@ -11,8 +12,8 @@ import (
 // GivenExistingPURL ensures that a PURL is known to the application.
 //
 // This is done by simply creating it.
-func GivenExistingPURL(t *testing.T, service AdminAPI, purl *PURL) {
-	path, err := service.SavePURL(purl)
+func GivenExistingPURL(ctx context.Context, t *testing.T, service AdminAPI, purl *PURL) {
+	path, err := service.SavePURL(ctx, purl)
 	require.NoError(t, err, "saving purl failed")
 	require.NotEmpty(t, path)
 }
@@ -20,13 +21,13 @@ func GivenExistingPURL(t *testing.T, service AdminAPI, purl *PURL) {
 // GivenExistingDomain ensures that a Domain is known to the application.
 //
 // This currently is a no-op since domains can't explicitly be created.
-func GivenExistingDomain(t *testing.T, service AdminAPI, domain string) {
-	err := service.CreateDomain(domain)
+func GivenExistingDomain(ctx context.Context, t *testing.T, service AdminAPI, domain string) {
+	err := service.CreateDomain(ctx, domain)
 	require.NoError(t, err, "creating domain failed")
 }
 
 // GivenSomeUser creates a user and returns the key for it.
-func GivenSomeUser(t *testing.T, userService *app.UserService) *models.UserKey {
+func GivenSomeUser(_ context.Context, t *testing.T, userService *app.UserService) *models.UserKey {
 	user, err := userService.CreateUser("test@local.com")
 	require.NoError(t, err)
 
