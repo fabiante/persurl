b9f38351b49a91498eeed33c92ecb55d016d300c
fabiante <fabiante@users.noreply.github.com>
2023-09-24 12:34:56 +0200

    Add Context object to test APIs

--- tests/dsl/apis.go
+++ tests/dsl/apis.go
@@ -1,22 +1,25 @@
 package dsl
 
-import "net/url"
+import (
+	"context"
+	"net/url"
+)
 
 // AdminAPI defines admin features of the application.
 type AdminAPI interface {
-	CreateDomain(name string) error
+	CreateDomain(ctx context.Context, name string) error
 
 	// SavePURL creates a new or updates an existing purl.
 	//
 	// If no error occurred the returned string is the path (without host) of the created PURL.
-	SavePURL(purl *PURL) (string, error)
+	SavePURL(ctx context.Context, purl *PURL) (string, error)
 }
 
 // ResolveAPI defines features for PURL resolution.
 type ResolveAPI interface {
 	// ResolvePURL resolves the PURL identified by domain and name, returning
 	// the target of the resolved PURL.
-	ResolvePURL(domain string, name string) (*url.URL, error)
+	ResolvePURL(ctx context.Context, domain string, name string) (*url.URL, error)
 }
 
 // API aggregates all apis.
